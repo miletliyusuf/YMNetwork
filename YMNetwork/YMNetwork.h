77afd64368e549ef876c82f34736ec91711b28b3
Yusuf Miletli <miletliyusuf@gmail.com>
2020-04-12 13:39:59 +0300

    Create initially framework

---
+++ YMNetwork/YMNetwork.h
@@ -0,0 +1,19 @@
+//
+//  YMNetwork.h
+//  YMNetwork
+//
+//  Created by Yusuf Miletli on 12.04.2020.
+//  Copyright © 2020 Miletli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMNetwork.
+FOUNDATION_EXPORT double YMNetworkVersionNumber;
+
+//! Project version string for YMNetwork.
+FOUNDATION_EXPORT const unsigned char YMNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMNetwork/PublicHeader.h>
+
+
---
+++ YMNetwork/YMNetwork.swift
@@ -0,0 +1,14 @@
+//
+//  YMNetwork.swift
+//  YMNetwork
+//
+//  Created by Yusuf Miletli on 12.04.2020.
+//  Copyright © 2020 Miletli. All rights reserved.
+//
+
+import Foundation
+
+public func isSuccess() {
+
+    print("Success")
+}
