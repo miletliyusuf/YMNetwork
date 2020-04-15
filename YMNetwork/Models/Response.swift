553e8b2dadf1867d51c29bea2a0f4d035ffec5b3
Yusuf Miletli <miletliyusuf@gmail.com>
2020-04-15 22:32:55 +0300

    Add response models

---
+++ YMNetwork/Models/Response.swift
@@ -0,0 +1,49 @@
+//
+//  Response.swift
+//  YMNetwork
+//
+//  Created by Yusuf Miletli on 15.04.2020.
+//  Copyright © 2020 Miletli. All rights reserved.
+//
+
+import Foundation
+
+public enum NetworkResponse: String {
+
+    case success
+    case authenticationError = "You need to be authenticated first."
+    case badRequest = "Bad Request"
+    case outdated = "The url you requested is outdated."
+    case failed = "Network requst failed."
+    case noData = "Response returned with no data to decode."
+    case unableToDecode = "We could not decode the response."
+}
+
+public enum Result<Value> {
+
+    case success(Value)
+    case failure(NetworkResponse)
+}
+
+public struct Response {
+
+    public var response: HTTPURLResponse?
+    public var data: Data?
+
+    public init(
+        response: HTTPURLResponse?,
+        data: Data?
+    ) {
+
+        self.response = response
+        self.data = data
+    }
+}
+
+// Codable response protocol that conforms to Codable
+public protocol CodableResponse: Codable {}
+
+public extension CodableResponse {
+
+    func encode(to encoder: Encoder) throws {}
+}
